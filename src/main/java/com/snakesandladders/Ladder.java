82da392882a101ebc46975fd84a4ae7ec74364e5
Khushboo <khushbooC1999@gmail.com>
2025-07-03 14:39:55 +0530

    feat: Add Snake and Ladder classes for Board

---
+++ src/main/java/com/snakesandladders/Ladder.java
@@ -0,0 +1,19 @@
+package snakeandladders;
+
+public class Ladder {
+    private final int start;
+    private final int end;
+
+    public Ladder(int start, int end) {
+        if (start >= end) throw new IllegalArgumentException("Invalid Ladder");
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() { 
+        return start; 
+    }
+    public int getEnd() { 
+        return end;
+    }
+}
---
+++ src/main/java/com/snakesandladders/Snake.java
@@ -0,0 +1,19 @@
+package snakeandladders;
+
+public class Snake {
+    private final int head;
+    private final int tail;
+
+    public Snake(int head, int tail) {
+        if (tail >= head) throw new IllegalArgumentException("Invalid Snake");
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public int getHead() {
+        return head;
+        }
+    public int getTail() { 
+        return tail; 
+        
+    }
