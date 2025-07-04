4b36507c1dbc61e4aa1d0d8d5fdb3e429da8179a
Khushboo <khushbooC1999@gmail.com>
2025-07-04 09:58:37 +0530

    feat: Add Player Class for the game

---
+++ src/main/java/com/snakesandladders/Player.java
@@ -0,0 +1,27 @@
+package snakeandladders;
+
+public class Player {
+    private final String name;
+    private int position;
+
+    public Player(String name) {
+        this.name = name;
+        this.position = 0; // Start at position 0
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void moveTo(int newPosition) {
+        this.position = newPosition;
+    }
+
+    public boolean hasWon() {
+        return position == 100;
+    }
+}
